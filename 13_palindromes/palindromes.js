e902a690c664fab73784f55aa109d930845905ed
“ClutchMils” <220502194+ClutchMils@users.noreply.github.com>
2025-11-10 13:39:18 -0500

    javascript exercise: palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    // Remove non-alphanumeric characters and convert to lowercase
+     const cleanedStr = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    
+     // Check if the cleaned string is equal to its reverse
+    const reversedStr = cleanedStr.split('').reverse().join('');
+    return cleanedStr === reversedStr;
 };
 
 // Do not edit below this line
