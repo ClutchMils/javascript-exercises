9a33adc4d6d0b925e1cfa5423c3e0309fbae2c34
“ClutchMils” <220502194+ClutchMils@users.noreply.github.com>
2025-11-11 06:28:02 -0500

    javascript exercise: find the oldest in an array

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+let currentYear = new Date().getFullYear();
+let oldestPerson = array.reduce((oldest, person) => {
+    let deathYear = person.yearOfDeath ? person.yearOfDeath : currentYear;
+    let age = deathYear - person.yearOfBirth;
+    let oldestDeathYear = oldest.yearOfDeath ? oldest.yearOfDeath : currentYear;
+    let oldestAge = oldestDeathYear - oldest.yearOfBirth;
+    return age > oldestAge ? person : oldest;
+});
+return oldestPerson;
 };
 
 // Do not edit below this line
